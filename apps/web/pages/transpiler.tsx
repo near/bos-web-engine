cbf3de82f1265ecb552e100651397f5908700232
Andy Haynes <andy@near.org>
2023-08-11 12:56:49 -0700

    feat: better source parsing

--- apps/web/pages/transpiler.tsx
+++ apps/web/pages/transpiler.tsx
@@ -46,11 +46,10 @@ export default function Transpiler() {
     }
 
     function parseChildWidgetPaths(transpiledWidget) {
-      const widgetRegex = /h\\(Widget,\\s+\\{(?:\\s*src:\\s+(?<source0>[\\w\\d '".\\/]+))?,?\\s*(?:props:\\s*\\s*(?<props>(?:\\w+|{\\s*[\\w\\s.,":_-]+})))(?:\\s*,?(?:\\s*src:\\s+(?<source1>[\\w\\d'".\\/]+))?)/gi;
+      const widgetRegex = /h\\(Widget,\\s*\\{(?:[\w\W]*?)(?:\\s*src:\\s+["|'](?<src>[\\w\\d_]+\\.near\\/widget\\/[\\w\\d_.]+))["|']/ig;
       const matches = [...(transpiledWidget.matchAll(widgetRegex))]
         .reduce((widgetInstances, match) => {
-          const { source0, source1 } = match.groups;
-          const source = (source0 || source1).replace(/["']/ig, '');
+          const source = match.groups.src;
           widgetInstances[source] = {
             source,
             transform: (widgetSource, widgetComponentName) => widgetSource.replaceAll(match[0], match[0].replace('Widget', widgetComponentName))
