e918cf5d4aa152e6e68fff181273925c622cc56b
Michael Peter <m.peter.dev@gmail.com>
2023-10-02 12:04:08 -0400

    lint:fix

--- apps/web/pages/flags.tsx
+++ apps/web/pages/flags.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
-import type { SubmitHandler } from "react-hook-form";
-import { useForm } from "react-hook-form";
-import styled from "styled-components";
+import { useEffect } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
 
-import { useFlags } from "../hooks/useFlags";
+import { useFlags } from '../hooks/useFlags';
 
 const Container = styled.div`
   display: flex;
@@ -38,7 +38,7 @@ export default function FlagsPage() {
   const form = useForm<FormData>();
 
   useEffect(() => {
-    form.setValue("bosLoaderUrl", flags?.bosLoaderUrl || "");
+    form.setValue('bosLoaderUrl', flags?.bosLoaderUrl || '');
   }, [form, flags]);
 
   const submitHandler: SubmitHandler<FormData> = (data) => {
@@ -57,11 +57,11 @@ export default function FlagsPage() {
             className="form-control"
             placeholder="e.g. http://127.0.0.1:3030/, https://my-loader.ngrok.io"
             id="bosLoaderUrl"
-            {...form.register("bosLoaderUrl")}
+            {...form.register('bosLoaderUrl')}
           />
         </InputGrid>
 
-        <button type="submit" style={{ marginLeft: "auto" }}>
+        <button type="submit" style={{ marginLeft: 'auto' }}>
           Save Flags
         </button>
       </Form>
