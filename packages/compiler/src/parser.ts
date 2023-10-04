a2e065d83e7bfbf63c4bff641ebec909667622bf
Andy Haynes <andy@near.org>
2023-10-04 10:55:34 -0700

    feat: parse isTrusted flag

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -1,33 +1,48 @@
-export function parseChildComponentPaths(transpiledComponent: string) {
+function parseWidgetRenders(transpiledComponent: string) {
+  const functionOffset = 'createElement'.length;
   const componentRegex =
     /createElement\(Widget,\s*\{(?:[\w\W]*?)(?:\s*src:\s*["|'](?<src>((([a-z\d]+[\-_])*[a-z\d]+\.)*([a-z\d]+[\-_])*[a-z\d]+)\/widget\/[\w.]+))["|']/gi;
-  const matches = [...transpiledComponent.matchAll(componentRegex)].reduce(
-    (componentInstances, match) => {
-      if (!match.groups?.src) {
-        return componentInstances;
+  return [...transpiledComponent.matchAll(componentRegex)].map((match) => {
+    const openParenIndex = match.index! + functionOffset;
+    let parenCount = 1;
+    let idx = openParenIndex + 1;
+    while (parenCount > 0) {
+      const char = transpiledComponent[idx++];
+      if (char === '(') {
+        parenCount++;
+      } else if (char === ')') {
+        parenCount--;
       }
+    }
 
-      const source = match.groups?.src;
-      componentInstances[source] = {
+    return {
+      expression: transpiledComponent.substring(
+        openParenIndex - functionOffset,
+        idx
+      ),
+      source: match.groups?.src || '',
+    };
+  });
+}
+
+export function parseChildComponentPaths(transpiledComponent: string) {
+  return parseWidgetRenders(transpiledComponent).map(
+    ({ expression, source }) => {
+      const [trustMatch] = [
+        ...expression.matchAll(/isTrusted(?:\s*:\s*(true|false))/gi),
+      ];
+
+      return {
         source,
+        isTrusted: trustMatch?.[1] === 'true',
         transform: (componentSource: string, componentName: string) => {
           const signaturePrefix = `${componentName},{__bweMeta:{parentMeta:props.__bweMeta},`;
           return componentSource.replaceAll(
-            match[0],
-            match[0].replace(/Widget,\s*\{/, signaturePrefix)
+            expression,
+            expression.replace(/Widget,\s*\{/, signaturePrefix)
           );
         },
       };
-
-      return componentInstances;
-    },
-    {} as {
-      [key: string]: {
-        source: string;
-        transform: (s: string, n: string) => string;
-      };
     }
   );
-
-  return Object.values(matches);
 }
