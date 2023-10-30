5744db8266e93060d21f0c898e3c41e40e5cd79e
Andy Haynes <andy@near.org>
2023-10-30 15:18:49 -0700

    fix: types

--- packages/compiler/src/transpile.ts
+++ packages/compiler/src/transpile.ts
@@ -4,7 +4,9 @@ import Babel from '@babel/standalone';
  * Transpile the Component's JSX source code, replacing React.createElement with `createElement`
  * @param source Component function source code using JSX
  */
-export function transpileSource(source: string) {
+export function transpileSource(source: string): {
+  code?: string | null;
+} {
   return Babel.transform(source, {
     plugins: [
       [
