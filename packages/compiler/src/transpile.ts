ceb2655aa858f132f673f042fcbe53c8b59c335c
Michael Peter <hi@mpeter.us>
2023-11-03 13:33:23 -0400

    support TypeScript

--- packages/compiler/src/transpile.ts
+++ packages/compiler/src/transpile.ts
@@ -8,11 +8,13 @@ export function transpileSource(source: string): {
   code?: string | null;
 } {
   return Babel.transform(source, {
+    presets: [Babel.availablePresets['typescript']],
     plugins: [
       [
         Babel.availablePlugins['transform-react-jsx'],
         { pragma: 'createElement' },
       ],
     ],
+    filename: 'component.tsx', // name is not important, just the extension
   });
 }
