bce06c17915908e7504beebaa7e009343ffa1da9
Caleb Jacob <thecalebjacob@gmail.com>
2024-01-10 17:13:09 -0700

    Adding note about disabling SSR for <Sandbox />

--- packages/sandbox/src/components/Sandbox.tsx
+++ packages/sandbox/src/components/Sandbox.tsx
@@ -30,6 +30,14 @@ export function Sandbox() {
   const [shouldRender, setShouldRender] = useState(false);
 
   useEffect(() => {
+    /*
+      This prevents Next JS from attempting to render this component on 
+      the server. Without this logic, Next JS complains about things like
+      `className` not matching what's rendered on the server. Base on 
+      current assumptions, rendering this component via SSR wouldn't make 
+      much sense anyways.
+    */
+
     setShouldRender(true);
   }, []);
 
