ce01ef263d9d8eec760232f78e8bff03f04cf831
Caleb Jacob <thecalebjacob@gmail.com>
2024-02-15 11:33:56 -0700

    Remove bootstrap (#285)

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@bos-web-engine/ui/reset.css';
 import '@bos-web-engine/ui/styles.css';
 import '@bos-web-engine/wallet-selector-control/styles.css';
@@ -15,24 +13,15 @@ import {
 import type { WalletSelector } from '@near-wallet-selector/core';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { MAINNET_WALLET_SELECTOR_PARAMS } from '@/constants';
 import s from '@/styles/app.module.css';
-
-/*
-  TODO: Can we remove all of the Bootstrap CSS and library dependencies eventually? 
-  The global Bootstrap styles cause all kinds of styling conflicts.
-*/
 
 export default function App({ Component, pageProps }: AppProps) {
   const [walletSelector, setWalletSelector] = useState<WalletSelector | null>(
     null
   );
-
-  useEffect(() => {
-    require('bootstrap/dist/js/bootstrap.bundle.min.js');
-  }, []);
 
   return (
     <WalletSelectorProvider
