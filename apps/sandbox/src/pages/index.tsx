c391c1c8bc33c2f7b77e2f4b2e4c56c62c1f18bd
Michael Peter <hi@mpeter.us>
2024-02-21 13:32:55 -0500

    change header height css var name

--- apps/sandbox/src/pages/index.tsx
+++ apps/sandbox/src/pages/index.tsx
@@ -5,7 +5,7 @@ import s from '@/styles/home.module.css';
 export default function Home() {
   return (
     <div className={s.wrapper}>
-      <Sandbox height="calc(100vh - var(--header-height))" />
+      <Sandbox height="calc(100vh - var(--gateway-header-height))" />
     </div>
   );
 }
