060714c4484bd7ffb72e5de139b83132c30351c7
Bucky <tom@kleingers.us>
2024-04-15 16:34:48 -0400

    resolves #364 browsertarget compatibility. (#408)

--- packages/compiler/src/css.ts
+++ packages/compiler/src/css.ts
@@ -1,4 +1,7 @@
+import browserslist from 'browserslist';
 import initCssWasm, {
+  browserslistToTargets,
+  Targets,
   transform as transformCss,
   TransformOptions,
   TransformResult,
@@ -7,6 +10,9 @@ import initCssWasm, {
 import type { ParsedCssModule } from './types';
 
 export class CssParser {
+
+  private browserTargets: Targets = browserslistToTargets(browserslist('> 0.5%, last 2 versions, Firefox ESR, not dead'));
+
   private lightningCssTransform:
     | ((options: TransformOptions<any>) => TransformResult)
     | undefined;
@@ -23,7 +29,9 @@ export class CssParser {
       code: new TextEncoder().encode(css),
       cssModules: true,
       filename: componentPath,
+      targets: this.browserTargets
     });
+
 
     return {
       classMap: new Map(
